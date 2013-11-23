27e939c234e56ae83e1f9b4227c497e9c3cebf64
Laurie Lugrin <laurie.lugrin@gmail.com>
2013-11-23 15:56:21 +0000

    Fixed-size cells.

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ var ascii_draw = (function() {
 
     var me = {};
 
+    var font_dimensions;
+
     me.addRow = function() {
         var drawingarea = document.getElementById('drawingarea');
         var row = drawingarea.insertRow();
         var length = drawingarea.rows[0].cells.length;
         for (var i = 0; i < length; i++) {
             var cell = row.insertCell();
-            cell.appendChild(document.createTextNode('b'));
+            cell.style.width = font_dimensions[0]+'px';
+            cell.style.height = font_dimensions[1]+'px';
+            cell.appendChild(document.createTextNode(' '));
         }
     };
 
@@ -21,17 +25,42 @@ var ascii_draw = (function() {
         for (var i = 0; i < length; i++) {
             var row = drawingarea.rows[i];
             var cell = row.insertCell();
-            cell.appendChild(document.createTextNode('b'));
+            cell.style.width = font_dimensions[0]+'px';
+            cell.style.height = font_dimensions[1]+'px';
+            cell.appendChild(document.createTextNode(''));
         }
     };
 
     me.onWindowLoad = function() {
-        for (var r = 0; r < 25; r++) {
+
+        font_dimensions = me.getFontDimensions();
+
+        // create cells in the drawing area table
+        for (var r = 0; r < 5; r++) {
             me.addRow();
         }
-        for (var c = 0; c < 80; c++) {
+        for (var c = 0; c < 8; c++) {
             me.addCol();
         }
+
+    };
+
+    me.getFontDimensions = function() {
+        var t = document.createElement('table');
+        var row = t.insertRow();
+        var cell = row.insertCell();
+        cell.appendChild(document.createTextNode('M'));
+        document.body.appendChild(t);
+
+        var size =
+            [
+                cell.offsetWidth,
+                cell.offsetHeight
+            ];
+
+        document.body.removeChild(t);
+
+        return size;
     };
 
     window.addEventListener('load', me.onWindowLoad, false);
