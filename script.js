52dea0aaa3e03f5c2168a79d996fec47fea22e71
Giovanni Funchal <gafunchal@gmail.com>
2013-11-23 11:19:42 +0000

    Add window load event.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 function addRow() {
-    drawingarea = document.getElementById("drawingarea");
+    drawingarea = document.getElementById('drawingarea');
     var cells = drawingarea.rows[0].cells;
     r = drawingarea.insertRow();
     for (var i = 0; i < cells.length; i++) {
         c = r.insertCell();
-        c.appendChild(document.createTextNode("b"));
+        c.appendChild(document.createTextNode('b'));
     }
 }
 
 function addCol() {
-    drawingarea = document.getElementById("drawingarea");
+    drawingarea = document.getElementById('drawingarea');
     var length = drawingarea.rows.length;
     for (var i = 0; i < length; i++) {
         c = drawingarea.rows[i].insertCell();
-        c.appendChild(document.createTextNode("b"));
+        c.appendChild(document.createTextNode('b'));
     }
 }
+
+function onWindowLoad() {
+    for (var i = 0; i < 25; i++) {
+        addRow();
+    }
+    for (var i = 0; i < 80; i++) {
+        addCol();
+    }
+}
+
+window.addEventListener('load', onWindowLoad, false);
